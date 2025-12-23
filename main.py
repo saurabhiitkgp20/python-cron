48069944e5d8ab6978a53e8a1a1a9b254d94bbfa
saurabhiitkgp20 <saurabh.iitkgp20@gmail.com>
2025-12-24 00:04:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import os
+import requests
 from datetime import datetime
 
-print("Program ran at:", datetime.now())
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+def send_telegram(msg):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": msg,
+        "parse_mode": "Markdown"
+    }
+    requests.post(url, data=payload)
+
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+send_telegram(f"✅ *Telegram bot working*\n⏰ Time: `{now}`")
